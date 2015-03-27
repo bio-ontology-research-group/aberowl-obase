99b2245ba8a88ee15498e2ee3fca7a644c09bd9a
Luke Slater <tinmachin3@gmail.com>
2015-03-28 01:02:55 +0300

    Update OntologyDatabase.groovy

--- OntologyDatabase.groovy
+++ OntologyDatabase.groovy
@@ -1,7 +1,5 @@
 /**
- * The OntologyDatabase handles a bunch of ontologies and their versions. 
- * 
- * For now it is a mock and doesn't really save anything. That's okay.
+ * The OntologyDatabase handles a bunch of ontologies and their versions. It's okay.'
  *
  */
 package db;
