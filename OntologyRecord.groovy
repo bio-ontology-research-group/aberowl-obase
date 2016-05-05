f979321a4fd946d156c406cc3685da65f424e442
Robert Hoehndorf <robert.hoehndorf@kaust.edu.sa>
2016-05-05 20:03:24 +0300

    some stuff

--- OntologyRecord.groovy
+++ OntologyRecord.groovy
@@ -14,6 +14,7 @@ import java.lang.reflect.Modifier
 import groovyx.net.http.ContentType
 import org.apache.commons.io.FileUtils
 import org.apache.commons.codec.digest.DigestUtils
+import java.nio.file.*
 
 class OntologyRecord {
   public final static String BASE_ONTOLOGY_DIRECTORY = 'onts/'
@@ -36,27 +37,37 @@ class OntologyRecord {
 
   void addNewSubmission(data) {
     def http = new HTTPBuilder()
+    http.getClient().getParams().setParameter("http.connection.timeout", new Integer(600*1000))
+    http.getClient().getParams().setParameter("http.socket.timeout", new Integer(600*1000))
     def fileName = id+'_'+(submissions.size()+1)+'.ont'
     def oFile = new File(BASE_ONTOLOGY_DIRECTORY+fileName)
     def tempFile = new File('/tmp/'+fileName)
 
     // Get the checksum of the most recent release.
-    def currentFile = new FileInputStream(new File(BASE_ONTOLOGY_DIRECTORY+submissions[lastSubDate]))
-    def oldSum = md5Hex(currentFile)
-    currentFile.close()
+    def oldSum = 0
+    try {
+      def currentFile = new FileInputStream(new File(BASE_ONTOLOGY_DIRECTORY+submissions[lastSubDate]))
+      oldSum = DigestUtils.md5Hex(currentFile)
+      currentFile.close()
+    } catch (Exception E) {
 
+    }
+
+    println "Downloading from "+data.download
     http.get('uri': data.download, 'contentType': ContentType.BINARY, 'query': [ 'apikey': API_KEY ] ) { 
       resp, ontology ->
         FileUtils.copyInputStreamToFile(ontology, tempFile)
-        def newSum = md5Hex(new FileInputStream(tempFile))
+        def newSum = DigestUtils.md5Hex(new FileInputStream(tempFile))
 
         if(oldSum != newSum) {
-          FileUtils.moveFile(tempFile, oFile)
+	  //FileUtils.moveFile(tempFile, oFile)
+	  /* pretty dangerous: */
+	  Files.move(tempFile.toPath(),oFile.toPath(),StandardCopyOption.REPLACE_EXISTING)
           lastSubDate = data.released
           submissions[data.released] = fileName
         }
-        oFile.close()
-        tempFile.close()
+	//	oFile.close()
+	//        tempFile.close()
     }
   }
 
